d8a4728c9928e21a09ff75fc4f55ca433c68e4ad
ReadyAndroid <rahul.androidnewsletter@gmail.com>
2018-01-09 19:54:35 +0530

    Added files to use for database upgrade
    Added files to use for database upgrade if any changes relative to the existing app database with manual code on the app version upgrade

---
+++ UpgradeDatabase/ColumnInfo.java
@@ -0,0 +1,9 @@
+package com.readyandroid.database;
+
+public class ColumnInfo {
+        public String name;
+        public String datatype;
+        public String defaultValue;
+        public boolean isNotNull;
+        public boolean isPrimaryKey;
+    }
